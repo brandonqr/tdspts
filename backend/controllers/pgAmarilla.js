e40eebcf7fe83139c240d290660c1f23ec0c5629
develooper24 <brandonqr1@gmail.com>
2018-03-26 18:56:50 +0200

    Paginas Amarillas 3.0

--- backend/controllers/pgAmarilla.js
+++ backend/controllers/pgAmarilla.js
@@ -1,273 +1,269 @@
-const Nightmare = require("nightmare");
+var Nightmare = require("nightmare");
 var Web = require("../models/web");
-
 class PgAmarilla {
     constructor() {
-            this.busqueda = "";
-            this.nightmare = Nightmare({ show: true });
-            this.data = {};
-            this.todosLosLinks = [];
-            this.haEntradoEnUrlSugerencia = false;
-            this.url = '';
-            this.links = [];
-            this.contadorObjeto = 0;
-            this.contadorTodosLosLinks = 0;
-            this.contador = 0;
-        }
-        //solo busca y obtiene el total de resultado, el numero de paginas, etc
-    BuscarEnWeb(busqueda) {
-            console.log(`Buscando páginas con la busqueda ${busqueda}`)
-            this.busqueda = busqueda;
+        this.nightmare = Nightmare({ show: true });
+        this.todosLosLinks = [];
+        this.contadorResultados = 0;
+        this.contadorObjetos = 0;
+        this.linksObjetos = [];
+        this.web = {};
+    }
+    Busqueda(busqueda) {
+        console.log("Incio");
+        var that = this;
+        this.nightmare
+            .goto("https://www.paginasamarillas.es/")
+            .type("#whatInput", busqueda)
+            .click("#submitBtn")
+            .wait(".listado-item")
+            .inject("js", "node_modules/jquery/dist/jquery.min.js")
+            .evaluate(() => {
+                //Obtener urlPaginaActual y urlPaginaSugerencia.
+                var url = window.location.href;
+                var urlSugerencia = $(
+                    "div.content:nth-child(2) > p:nth-child(1) > a:nth-child(1)"
+                ).attr("href");
+                var objeto = $("body").attr("data-analytics");
+                return { url, urlSugerencia };
+            })
+            //.end()
+            .then(objeto => {
+                //ObtenerNPaginasUrl(objeto.url).then((value) => { console.log(value) });
+                //ObtenerNPaginasUrl(objeto.urlSugerencia).then((value) => { console.log(value) });
+                var url = "";
+                if (objeto.urlSugerencia != null) {
+                    Promise.all([
+                        this.ObtenerNPaginasUrl(objeto.url),
+                        this.ObtenerNPaginasUrl(objeto.urlSugerencia)
+                    ]).then(function(arrayOfResults) {
+                        for (let i = 0; i < arrayOfResults.length; i++) {
+                            that.GenerarLinks(
+                                arrayOfResults[i].url,
+                                arrayOfResults[i].nPaginas
+                            );
+                        }
+                        that.RecorrerResultados(
+                            that.todosLosLinks[that.contadorResultados]
+                        );
+                    });
+                } else {
+                    this.ObtenerNPaginasUrl(objeto.url).then(result => {
+                        this.GenerarLinks(result.url, result.nPaginas);
+                        this.RecorrerResultados(
+                            this.todosLosLinks[this.contadorResultados]
+                        );
+                    });
+                }
 
+                //console.log(objeto)
+            })
+            .catch(error => {
+                //  console.error("Search failed:", error);
+                console.log("ha petado en busqueda");
+            });
+    }
+    ObtenerNPaginasUrl(url) {
+        console.log("Obtener N Paginas Url");
+        return new Promise((resolve, reject) => {
             this.nightmare
-                .goto("https://www.paginasamarillas.es/search")
-                .type("#what", this.busqueda)
-                .click("#btnFind")
+                .goto(url)
                 .wait(".listado-item")
                 .inject("js", "node_modules/jquery/dist/jquery.min.js")
                 .evaluate(() => {
-                    //obtener variables globales como nResults,paginas, etc
-                    var data = {};
                     var objeto = $("body").attr("data-analytics");
                     objeto = JSON.parse(objeto);
                     var nResults = objeto.search.numResults;
                     var itemsPorPagina = objeto.products.length;
-                    var nPaginas = Math.ceil(parseInt(nResults) / parseInt(itemsPorPagina));
+                    var nPaginas = Math.ceil(
+                        parseInt(nResults) / parseInt(itemsPorPagina)
+                    );
                     var url = window.location.href;
-                    var urlSugerencia = $(
-                        "div.content:nth-child(2) > p:nth-child(1) > a:nth-child(1)"
-                    ).attr("href");
-                    data = {
-                        nResults,
-                        itemsPorPagina,
-                        nPaginas,
-                        url,
-                        urlSugerencia
-                    };
-                    return data;
+                    return { nPaginas, url };
                 })
-                //.end()
-                .then(data => {
-                    this.data = data;
-                    this.ObtenerTodosLosLinks();
+                // .end()
+                .then(objeto => {
+                    resolve(objeto);
+                    console.log(objeto);
                 })
                 .catch(error => {
-                    console.error("Search failed:", error);
+                    reject("Ha ocurrido un error en obtener Npaginas url");
+                    //console.log("ha petado en busqueda");
+                    //console.error("Search failed:", error);
                 });
-        }
-        //ObtenerTodosLosLinks hace un push a la variable this.todosLosLinks de todas las url de la busqueda
-        //y tambien de todas las urls de sugerencias.
-    ObtenerTodosLosLinks() {
-        console.log('Obteniendo los links');
-
-        //la variable haEntradoEnUrlSugerencia de activa cuando se ejecuta la funcion ActualizarDatos
-        //sólo entro 1 vez en url sugerencia, porque siempre se repite (Error de la web)
-        if (!this.haEntradoEnUrlSugerencia) {
-            this.GenerarLinks(this.data.url);
-            //si hay urlSugerencias, ejecutamos el if de abajo
-            if (this.data.urlSugerencia != null) {
-                this.ActualizarDatos();
-            } else {
-                this.ObtenerLinksUrlResultados();
-            }
-
-        }
-        //saliendo del if ya tendré todos los links de la busqueda en la variable this.todosLosLinks
-        //Inicializó el primer url
-
-
+        });
     }
-    ObtenerLinksUrlResultados() {
+    GenerarLinks(url, nPaginas) {
+        console.log("Generar Links");
+        console.log(`url ${url} y nPaginas: ${nPaginas}`);
+
+        // Funciones y variables para poder partir la url, para poder generarlas bien
+        var campos = url.split("/");
+        var primeraParte = "";
+        for (var i = 0; i < campos.length - 1; i++) {
+            primeraParte += campos[i] + "/";
+        }
+        //var numero = campos[campos.length - 1].split("?")[0];
+        var segundaParte = campos[campos.length - 1].split("?")[1];
+        //for para llenar el array this.todosLosLinks
+        for (var j = 1; j < nPaginas + 1; j++) {
+            this.todosLosLinks.push(primeraParte + j + "?" + segundaParte);
+        }
+    }
+    RecorrerResultados(url) {
+        console.log(
+            `Descargando  ${this.contadorResultados} de  ${this.todosLosLinks.length}`
+        );
         this.nightmare
-            .goto(this.todosLosLinks[this.contadorTodosLosLinks])
-            .wait(".listado-item")
+            .goto(url)
+            //.type("#search_form_input_homepage", "github nightmare")
+            //.click("#search_button_homepage")
             .inject("js", "node_modules/jquery/dist/jquery.min.js")
+            .wait(".central")
+            .wait(1000)
             .evaluate(() => {
-                //obtener variables globales como nResults,paginas, etc
                 var links = [];
-                var objeto = $("body").attr("data-analytics");
-                objeto = JSON.parse(objeto);
-                //llenar
-                $(".envio-consulta a").each(function() {
+
+                $("div.listado-item .envio-consulta a").each(function() {
                     links.push($(this).attr("href"));
                 });
                 return links;
             })
             //.end()
             .then(links => {
-                this.links = links;
+                this.linksObjetos = links;
+                /*
+                                                this.linksResultados = links;
+                                                this.contadorObjeto = 0;
+                                                this.urlObjeto = this.linksResultados[this.contadorObjeto];
+                                                */
+                //insertar
 
-                this.ObtenerObjeto();
-                console.log(`Guardando link ${this.contador+1} de ${this.todosLosLinks.length}`);
-                this.contador++;
+                if (this.linksObjetos[this.contadorObjetos] != null) {
+                    this.ObtenerObjeto(this.linksObjetos[this.contadorObjetos]);
+                    this.contadorResultados++;
+                    //
+
+                    //this.ObtenerObjeto();
+                } else {
+                    this.Finalizar();
+                }
             })
             .catch(error => {
-                console.error("Search failed:", error);
-            });
+                //console.log("ha petado en Recorrer Resultados");
+                this.Finalizar();
 
+                //console.error("Search failed:", error);
+            });
     }
-    ObtenerObjeto() {
+    ObtenerObjeto(url) {
         this.nightmare
-            .goto(this.links[this.contadorObjeto])
-            .inject("js", "node_modules/jquery/dist/jquery.min.js")
+
+            .goto(url)
+            .inject("js", "./node_modules/jquery/dist/jquery.min.js")
             .wait("#bloqueInfo")
             .evaluate(() => {
                 var servicios = [];
                 var redesSociales = [];
-                var objeto = {};
-                objeto = $("#toTop")
+                var objeto = $("#toTop")
                     .next()
                     .attr("data-business");
-                objeto = JSON.parse(objeto);
-                var web = {};
 
-                //obtener los servicios
                 $("#bloqueInfo li").each(function() {
                     servicios.push($(this).text());
                 });
+
                 //obtener las redes sociales
                 $(".socialNetworks li a").each(function() {
                     var socialObj = {};
                     socialObj.nombre = $(this).attr("class");
                     socialObj.link = $(this).attr("href");
-
                     redesSociales.push(socialObj);
                 });
-                //obtener el objeto que será insertado en la base de datos
-                web.email = objeto.customerMail || "";
-                web.actividad = objeto.info.activity || "";
-                web.direccion = objeto.info.businessAddress || "";
-                web.funte = $(".logo").attr("title");
-                web.pais = "España";
-                web.estado_mailchimp = 0;
-                web.usuario = "ADMIN";
-                web.fuente_id = objeto.info.id || "";
-                web.nombre = objeto.info.name || "";
-                web.telefono = objeto.info.phone || "";
-                web.localidad = objeto.location.locality || "";
-                web.provincia = objeto.location.province || "";
-                web.web = objeto.mapInfo.adWebEstablecimiento || "";
-                web.servicios = JSON.stringify(servicios);
-                web.redes_sociales = JSON.stringify(redesSociales);
 
+                redesSociales = JSON.stringify(redesSociales);
+
+                return { objeto, servicios, redesSociales };
+            })
+            //.end()
+            .then(data => {
+                data.objeto = JSON.parse(data.objeto);
+                var web = {};
+
+                web = {
+                    fuente: "Paginas Amarillas",
+                    fuente_id: data.objeto.info.id || "",
+                    nombre: data.objeto.info.name || "",
+                    actividad: data.objeto.info.activity || "",
+                    telefono: data.objeto.info.phone || "",
+                    email: data.objeto.customerMail || "",
+                    web: data.objeto.mapInfo.adWebEstablecimiento || "",
+                    provincia: data.objeto.location.province || "",
+                    localidad: data.objeto.location.locality || "",
+                    pais: "España",
+                    direccion: data.objeto.info.businessAddress || "",
+                    servicios: data.servicios,
+                    redes_sociales: data.redesSociales
+                };
                 return web;
             })
             .then(web => {
-                this.web = web;
-                if (this.contadorTodosLosLinks < this.todosLosLinks.length - 1 || this.todosLosLinks.length == 1) {
-                    if (this.contadorObjeto < this.links.length - 1 || this.todosLosLinks.length == 1) {
-                        this.InsertarEnDB();
+                web = web;
+                //console.log(links[contadorObjetos]);
+                console.log(`Guardando Empresa ${web.nombre}`);
+                this.InsertarEnDB(web);
 
-                        if (this.data.nResults != 1) {
-                            this.contadorObjeto++;
-                            this.ObtenerObjeto();
-                        }
+                // InsertarEnDB();
+                this.contadorObjetos++;
 
-
-                    } else {
-                        //reincio la variable this.contadorObjeto a 0, para volver a empezar
-                        this.contadorObjeto = 0;
-                        //cambiamos a la siguiente url
-                        this.contadorTodosLosLinks++;
-                        //y vuelvo a ejecutar la función recursivamente
-                        this.ObtenerLinksUrlResultados();
-                    }
+                if (this.linksObjetos[this.contadorObjetos] != null) {
+                    //console.log(this.urlObjeto);
+                    this.ObtenerObjeto(this.linksObjetos[this.contadorObjetos]);
                 } else {
-                    //finalizar
-                    this.Finalizar();
+                    //ir a por el siguiente urlResultados
+                    this.contadorObjetos = 0;
+                    this.RecorrerResultados(this.todosLosLinks[this.contadorResultados]);
                 }
-                if (this.data.nResults == 1) {
-                    this.Finalizar();
-                }
-
-                //insertar en la base de datos
             })
             .catch(error => {
-                console.error("Search failed:", error);
+                console.log("ha petado en obtener objeto");
+
+                // console.error("Search failed:", error);
             });
+    }
+    InsertarEnDB(web) {
+        var web = new Web(web);
+        //comprueba si el Objeto está insertado en la base de datos
+        Web.findOne({ fuente_id: web.fuente_id }, function(err, webAbuscar) {
+            if (err) {
+                return;
+            }
+            if (!webAbuscar) {
+                //Si el objeto no existe, lo inserta en la DB
+                web.save((err, web) => {
+                    if (err) {
+                        return;
+                    }
+                    console.log(`Se ha insertado`);
+                });
+            } else {
+                console.log(`ya se encuentra en la base de datos`);
+            }
+
+            return true;
+        });
     }
     Finalizar() {
         this.nightmare
             //.goto(this.links[this.contadorLinksObjeto])
             .end()
-            .then(() => { console.log('Ha finalizado') })
-            .catch(error => {
-                console.error("Search failed:", error);
-            });
-    }
-    InsertarEnDB() {
-
-            var web = new Web(this.web);
-            //comprueba si el Objeto está insertado en la base de datos
-            Web.findOne({ fuente_id: this.web.fuente_id }, function(
-                err,
-                webAbuscar
-            ) {
-                if (err) {
-                    return;
-                }
-                if (!webAbuscar) {
-                    //Si el objeto no existe, lo inserta en la DB
-                    web.save((err, web) => {
-                        if (err) {
-                            return;
-                        }
-                        console.log(`Se ha insertado`);
-                    });
-                } else {
-                    console.log(`ya se encuentra en la base de datos`);
-                }
-
-                return true;
-            });
-        }
-        //actualiza la varible this.datos
-    ActualizarDatos() {
-        this.nightmare
-            .goto(this.data.urlSugerencia)
-            //.type("#what", this.busqueda)
-            //.click("#btnFind")
-            .wait(".listado-item")
-            .inject("js", "node_modules/jquery/dist/jquery.min.js")
-            .evaluate(() => {
-                this.haEntradoEnUrlSugerencia = true;
-                //obtener variables globales como nResults,paginas, etc
-                var data = {};
-                var objeto = $("body").attr("data-analytics");
-                objeto = JSON.parse(objeto);
-                var nResults = objeto.search.numResults;
-                var itemsPorPagina = objeto.products.length;
-                var nPaginas = Math.ceil(parseInt(nResults) / parseInt(itemsPorPagina));
-                var url = window.location.href;
-                var urlSugerencia = $("div.content:nth-child(2) > p:nth-child(1) > a:nth-child(1)").attr("href");
-                data = { nResults, itemsPorPagina, nPaginas, url, urlSugerencia };
-                return data;
-            })
-            //.end()
-            .then(data => {
-                this.data = data;
-                this.GenerarLinks(this.data.url);
-                this.ObtenerLinksUrlResultados();
-                console.log(`${this.todosLosLinks.length} links obtenidos`)
+            .then(() => {
+                console.log("Ha finalizado");
             })
             .catch(error => {
-                console.error("Search failed:", error);
+                console.log("ha petado al finalizar");
+                //console.error("Search failed:", error);
             });
-    }
-    GenerarLinks(url) {
-        // Funciones y variables para poder partir la url, para poder generarlas bien
-        var campos = url.split('/');
-        var primeraParte = '';
-        for (let i = 0; i < campos.length - 1; i++) {
-            primeraParte += campos[i] + '/';
-        }
-        //var numero = campos[campos.length - 1].split("?")[0];
-        var segundaParte = campos[campos.length - 1].split("?")[1];
-        //for para llenar el array this.todosLosLinks
-        for (let j = 1; j < this.data.nPaginas + 1; j++) {
-            this.todosLosLinks.push(primeraParte + j + '?' + segundaParte);
-        }
-
     }
 }
 
